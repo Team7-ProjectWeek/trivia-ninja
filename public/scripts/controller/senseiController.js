261c63a606b1a86094857572bd3517416ebc57bd
Nicu Parente <nicuparente@gmail.com>
2017-05-30 14:26:34 -0700

    Stylized questions

--- public/scripts/controller/senseiController.js
+++ public/scripts/controller/senseiController.js
@@ -2,36 +2,36 @@
 
 var app = app || {};
 
-(function(module){
+(function (module) {
   const Sensei = {};
 
   Sensei.hasValidToken = (ctx, next) => {
-    if(!app.user.token || app.user.token.expirationTime <= Math.floor((new Date()).getTime() / 1000)){
+    if (!app.user.token || app.user.token.expirationTime <= Math.floor((new Date()).getTime() / 1000)) {
       console.log('Getting token now');
-      app.Sensei.tokenRequest(next); 
-    }else{
+      app.Sensei.tokenRequest(next);
+    } else {
       next();
     }
-    
+
   }
 
   Sensei.tokenRequest = (callback) => {
     $.get("https://opentdb.com/api_token.php?command=request")
-    .then((data)=> {
-      if(data.response_code === 0){
-         data.issueTime = Math.floor(new Date().getTime() / 1000);
-         data.expirationTime = data.issueTime + 21600; //21600 is 6 hours 
-         console.log(data);
-         app.user.token = data;
-         callback();
-      }else{
-        console.log("Invalid Token Request")
-      }
-      
-    },
-    (err) =>{
-      console.log(err);
-    });
+      .then((data) => {
+        if (data.response_code === 0) {
+          data.issueTime = Math.floor(new Date().getTime() / 1000);
+          data.expirationTime = data.issueTime + 21600; //21600 is 6 hours 
+          console.log(data);
+          app.user.token = data;
+          callback();
+        } else {
+          console.log("Invalid Token Request")
+        }
+
+      },
+      (err) => {
+        console.log(err);
+      });
   }
 
   Sensei.getQuestions = (ctx, next) => {
@@ -40,17 +40,11 @@ var app = app || {};
     app.Question.currentQuestionIndex = 0;
     $.get(url).then((data) => {
       app.Question.loadAll(data.results);
-      // app.Question.all.map((question) =>{
-      //   $('#theButton').parent().append(`<h1>${question.question}</h1>`);
-      // })
       app.stat.timeInit();
       app.QuestionView.serveQuestion();
     });
-
-    Sensei.nextQuestion = (ctx, next) => {
-
-    }
   }
+
 
 
   module.Sensei = Sensei;
