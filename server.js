bbc2c4b5dc131840a4379d9e4fe39d28c282141f
mikeporter <porter.michael.james@gmail.com>
2017-05-30 15:51:21 -0700

    added gitignore

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'))
 
+app.get('/game/*', tell you wehre to go)
+
 app.listen(PORT, function() {
   console.log(`You are running on PORT ${PORT}`)
 });
