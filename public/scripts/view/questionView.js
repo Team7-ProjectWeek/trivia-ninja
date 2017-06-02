02f35fc6c72ecfaf16d8a238684e5cb9e93013df
mikeporter <porter.michael.james@gmail.com>
2017-06-01 17:14:17 -0700

    fixed last answer not going green

--- public/scripts/view/questionView.js
+++ public/scripts/view/questionView.js
@@ -18,7 +18,9 @@ var app = app || {};
     $('.question-button').on('click', function () {
       if (app.Question.selectedAnswer != ''){
         if ($('.question-button').text() === 'DONE') {
-          app.statsController.completeGame();
+          app.Sensei.evaluateAnswer();
+          window.setTimeout(function(){
+            app.statsController.completeGame();}, 650);
         } else {
           if (app.Question.currentQuestionIndex < app.Question.all.length - 2) {
             app.Sensei.evaluateAnswer();
@@ -39,7 +41,7 @@ var app = app || {};
 
 
       $('.free-button').on('click', function () {
-        
+
         if (app.Question.selectedAnswer != ''){
           if (app.Question.currentQuestionIndex < app.Question.all.length - 1) {
             app.Sensei.evaluateAnswer();
